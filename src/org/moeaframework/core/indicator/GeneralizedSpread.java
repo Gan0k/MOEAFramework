1d8e266ac2d39e5de7d764f0059f3e3986a81068
Guido Arnau <guido.arnau@gmail.com>
2016-04-19 16:47:15 +0100

    Reindent Generalised Spread

--- src/org/moeaframework/core/indicator/GeneralizedSpread.java
+++ src/org/moeaframework/core/indicator/GeneralizedSpread.java
@@ -50,24 +50,25 @@ public class GeneralizedSpread extends NormalizedIndicator {
 	/**
 	 * Computes the maximum Pareto front error for the specified problem given
 	 * an approximation set and reference set. While not necessary, the
-	 * approximation and reference sets should be normalized. Returns
-	 * {@code Double.POSITIVE_INFINITY} if the approximation set is empty.
+	 * approximation and reference sets should be normalized. Returns {@code
+	 * Double.POSITIVE_INFINITY} if the approximation set is empty.
 	 *
 	 * @param problem the problem
 	 * @param approximationSet an approximation set for the problem
 	 * @param referenceSet the reference set for the problem
 	 * @return the generational distance for the specified problem given an
-	 *         approximation set and reference set
+	 *		   approximation set and reference set
 	 */
 	static double evaluate(Problem problem,
 			NondominatedPopulation approximationSet,
 			NondominatedPopulation referenceSet) {
 
 		if (approximationSet.isEmpty()) {
-			return 1.0;
+			return 2.0;
 		}
 
-		int numberOfObjectives = approximationSet.get(0).getNumberOfObjectives();
+		int numberOfObjectives =
+			approximationSet.get(0).getNumberOfObjectives();
 
 		Solution[] extremeValues = new Solution[numberOfObjectives];
 		for (int i = 0; i < numberOfObjectives; i++) {
@@ -78,25 +79,27 @@ public class GeneralizedSpread extends NormalizedIndicator {
 		approximationSet.sort(new LexicographicalComparator());
 
 		if (IndicatorUtils.euclideanDistance(problem,approximationSet.get(0),
-			approximationSet.get(approximationSet.size()-1)) == 0.0) {
+					approximationSet.get(approximationSet.size()-1)) == 0.0) {
 
-			return 1.0;
+			return 2.0;
 
 		} else {
 
 			double[] distances = new double[approximationSet.size()];
 
 			for (int i = 0; i < approximationSet.size(); i++) {
-				distances[i] = IndicatorUtils.distanceToNearestNeighbour(problem, i,
-						approximationSet);
+				distances[i] =
+					IndicatorUtils.distanceToNearestNeighbour(problem, i,
+							approximationSet);
 			}
 
 			double meanDistance = StatUtils.mean(distances);
 
 			double distExtremes = 0.0;
 			for (int i = 0 ; i < extremeValues.length; i++) {
-				distExtremes += IndicatorUtils.distanceToNearestSolution(problem,
-						extremeValues[i], approximationSet);
+				distExtremes +=
+					IndicatorUtils.distanceToNearestSolution(problem,
+							extremeValues[i], approximationSet);
 			}
 
 			double normalizedSum = 0.0;
